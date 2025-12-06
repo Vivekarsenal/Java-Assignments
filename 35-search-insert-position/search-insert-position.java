424d89c54ed1ae8d6500a19dc3f79020865ec99f
Vivek <88723599+Vivekarsenal@users.noreply.github.com>
2025-12-06 20:42:33 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (31.96%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l=0,r=nums.length-1;
+        int mid=0;
+        int ans=nums.length; // if the target is greater than the max elemet of the array
+
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(nums[mid]>=target){
+            ans=mid;
+            r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+}
