d31e5427d70654ba7bc6d386c0c64cfd735625f8
Vivek <88723599+Vivekarsenal@users.noreply.github.com>
2026-01-08 23:04:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (13.65%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
+    int n =digits.length;
 
-        // Traverse from last digit
-        for (int i = n - 1; i >= 0; i--) {
-            // If current digit is less than 9, just add 1 and return
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits; // no carry, done
-            }
-            // else set current to 0 and carry 1 to next
-            digits[i] = 0;
+    for(int i=n-1;i>=0;i--){
+        if(digits[i]<9){
+            digits[i]++;
+                return digits ;
         }
+          digits[i]=0;
+    }
+    
+   int nums[]=new int[n+1];
+   nums[0]=1;
+   return nums;
 
-        // If we reach here, it means all were 9s (like 999 -> 1000)
-        int[] newNumber = new int[n + 1];
-        newNumber[0] = 1;
-        return newNumber;
     }
 }
