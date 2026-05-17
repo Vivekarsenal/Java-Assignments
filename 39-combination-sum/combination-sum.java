116e141e5d106209580dfefc4f7f64c861c07830
Vivek <88723599+Vivekarsenal@users.noreply.github.com>
2026-05-18 01:53:26 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (81.44%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -4,39 +4,30 @@ class Solution {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-        List<List<Integer>> ds = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
+        findcombination(0,target,candidates,ans,list);
 
-        findcombination(0, target, candidates, ds, list);
-
-        return ds;
+        return ans;
     }
 
     private void findcombination(int idx,int target,int[] arr, List<List<Integer>> ans, List<Integer> list) {
 
-        // BASE CASE
-        if (idx == arr.length) {
-
-            if (target == 0) {
+        if(idx==arr.length){
+            if(target==0){
                 ans.add(new ArrayList<>(list));
             }
-
-            return;
+           return;
         }
 
-        // PICK
-        if (arr[idx] <= target) {
+        
 
+
+        if(arr[idx]<=target){
             list.add(arr[idx]);
-
-            // SAME INDEX because reuse allowed
-            findcombination(idx,target - arr[idx],arr,ans,list);
-
-            // BACKTRACK
-            list.remove(list.size() - 1);
+            findcombination(idx,target-arr[idx],arr,ans,list);
+            list.remove(list.size()-1);
         }
-
-        // NOT PICK
-        findcombination(idx + 1, target,arr, ans, list);
-    }
+        findcombination(idx+1,target,arr,ans,list);
+}
 }
