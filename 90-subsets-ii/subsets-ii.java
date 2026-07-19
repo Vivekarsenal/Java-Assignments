dbf903cd62669a755e117c035db0b22baf2522a4
Vivek <88723599+Vivekarsenal@users.noreply.github.com>
2026-07-19 14:54:52 +0530

    Time: 10 ms (7.35%) | Memory: 45.3 MB (10.42%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -6,13 +6,13 @@ class Solution {
         return subset;
     }
     private void backtrack(int[] nums,List<List<Integer>>subset,List<Integer>list,int start){
-        Arrays.sort(nums);
+         Arrays.sort(nums);
         if(subset.contains(list))return;
         subset.add(new ArrayList<>(list));
         
 
         for(int i=start;i<nums.length;i++){
-            
+            if(i>start && nums[i]==nums[i-1])continue;
             list.add(nums[i]);
             backtrack(nums,subset,list,i+1);
             
