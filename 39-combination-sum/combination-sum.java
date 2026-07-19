79074b24d28e87e61a05a5496280f445f3c8cacb
Vivek <88723599+Vivekarsenal@users.noreply.github.com>
2026-07-19 22:40:35 +0530

    Time: 3 ms (35.14%) | Memory: 45.8 MB (51.65%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,33 +1,32 @@
-import java.util.*;
-
 class Solution {
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+       List<List<Integer>> ans = new ArrayList<>();
+       List<Integer> list = new ArrayList<>();
 
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        findcombination(0,target,candidates,ans,list);
-
-        return ans;
+       combinations(candidates,ans,list,target,0,0);
+       return ans;
     }
-
-    private void findcombination(int idx,int target,int[] arr, List<List<Integer>> ans, List<Integer> list) {
-
-        if(idx==arr.length){
-            if(target==0){
-                ans.add(new ArrayList<>(list));
-            }
-           return;
+    private void combinations(int[] nums, List<List<Integer>>ans,List<Integer> list, int target, int sum,int start){
+        if(sum==target){
+            ans.add(new ArrayList<>(list));
+            return;
+            
         }
-
+        if (sum > target) {
+        return;
+       }
         
-
-
-        if(arr[idx]<=target){
-            list.add(arr[idx]);
-            findcombination(idx,target-arr[idx],arr,ans,list);
+       
+        for(int i=start;i<nums.length;i++){
+            
+           
+            list.add(nums[i]);
+            combinations(nums, ans ,list,target,sum+nums[i],i);
             list.remove(list.size()-1);
-        }
-        findcombination(idx+1,target,arr,ans,list);
+           
+
+            
+        
+    }
 }
 }
